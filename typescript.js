47e0f893e935a1c6d949410430dce90cced95319
Chris <chris@kcd.co.kr>
2021-03-08 17:21:11 +0900

    Update typescript.js

--- typescript.js
+++ typescript.js
@@ -1,14 +1,23 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: ['./tsconfig.json'],
-  },
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        sourceType: 'module',
+      },
+      plugins: ['@typescript-eslint', 'import'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:import/typescript',
+      ],
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-use-before-define': 'error',
+        'no-use-before-define': 'off',
+      },
+    }
   ],
-  rules: {
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-  },
 };
