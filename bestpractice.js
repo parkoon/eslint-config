e6358ea594dec5da1123d948aba7264d95263cc3
Heejae <chris@kcd.co.kr>
2021-03-29 15:21:09 +0900

    [bestpractice] function-call-argument-newline 추가 (#10)

--- bestpractice.js
+++ bestpractice.js
@@ -17,6 +17,12 @@ module.exports = {
 
     // 배열의 요소 사이 줄넘김을 일관되게 합니다.
     'array-element-newline': ['error', 'consistent'],
+    
+    // 함수 인자의 줄넘김을 일관되게 합니다.
+    'function-call-argument-newline': ['error', 'consistent'],
+
+    // 함수 인자 사이에 줄넘김이 있는 경우, 항상 괄호 안에 줄넘김이 필요합니다.
+    'function-paren-newline': ['error', 'multiline'],
 
     // multiline 일 때, 마지막 comma를 필수로 작성합니다.
     'comma-dangle': ['error', 'always-multiline'],
